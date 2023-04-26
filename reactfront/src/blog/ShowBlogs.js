f55685c45bcf7087cb724b04b47f410542b55dc6
Carlos Villegas <carlos.villegasch@gmail.com>
2023-04-26 18:56:14 -0500

    Logo modified size to 10vmin, import BrowserRouter, creation of structure to show blogs

--- reactfront/src/blog/ShowBlogs.js
+++ reactfront/src/blog/ShowBlogs.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-import {useState, useEffect} from react
-import {link} from 'react-router-dom'
+import {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
 
-const URI = 'http://localhost/8000/blogs/'
+const URI = 'http://localhost:8000/blogs/'
 
 const CompShowBlogs = () => {
      const [blogs, setBlog] = useState ([])
@@ -26,6 +26,7 @@ const CompShowBlogs = () => {
         <div className='container'>
             <div className='row'>
                 <div className='col'>
+                    <Link to="/create" className='btn btn-primary mt-2 mb-2'><i className="fa-solid fa-plus"></i></Link>
                     <table className='table'>
                         <thead className='table-primary'>
                             <tr>
@@ -37,11 +38,11 @@ const CompShowBlogs = () => {
                         <tbody>
                             { blogs.map ( (blog) => (
                                 <tr key={ blog.id}>
-                                    <td> {blog.title }</td>
+                                    <td> { blog.title }</td>
                                     <td> { blog.content }</td>
                                     <td>
-                                        <link to={`/edit/${blog.id}`} className="btn btn-info" > Editar </link>
-                                        <button onClick={ ()=>deleteBlog(blog.id)} className='btn btn-danger'>Delete</button>
+                                        <Link to={`/edit/${blog.id}`} className="btn btn-info" ><i className="fa-solid fa-pen-to-square"></i></Link>
+                                        <button onClick={ ()=>deleteBlog(blog.id)} className='btn btn-danger'><i className="fa-solid fa-trash-can"></i></button>
                                     </td>
                                 </tr>
                             ) )}
