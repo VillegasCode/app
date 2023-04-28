1eb173d545592508eec13a8a2ef634d9ab68e713
Carlos Villegas <carlos.villegasch@gmail.com>
2023-04-28 17:59:57 -0500

    Creation CompCreateBlog to button Create Blog work

---
+++ reactfront/src/blog/CreateBlog.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const URI = 'http://localhost:8000/blogs'
+
+const CompCreateBlog = () => {
+    return (
+        <div>
+            <h1>vista CREAR</h1>
+        </div>
+    )
+}
+
+export default CompCreateBlog
