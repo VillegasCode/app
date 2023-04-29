c7a8763fae5838c2d577ec43e40c4a8218b1211e
Carlos Villegas <carlos.villegasch@gmail.com>
2023-04-28 21:37:29 -0500

    Crear nuevo post

--- reactfront/src/blog/CreateBlog.js
+++ reactfront/src/blog/CreateBlog.js
@@ -1,13 +1,50 @@
-import axios from 'axios'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const URI = 'http://localhost:8000/blogs'
 
 const CompCreateBlog = () => {
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+    const navigate = useNavigate()
+
+    //Procedimiento guardar
+    const store = async (e) => {
+        e.preventDefault()
+        await axios.post(URI, {
+            title: title,
+            content: content})
+        navigate('/')
+    }
+
     return (
         <div>
-            <h1>vista CREAR</h1>
+            <h3>Create a new post</h3>
+            <form onSubmit={store}>
+                <div className='mb-3'>
+                    <label className='form-label'>Title</label>
+                    <input
+                        value={title}
+                        onChange={ (e)=> setTitle(e.target.value) }
+                        type="text"
+                        className='form-control'
+                        required
+                    />
+                </div>
+
+                <div className='mb-3'>
+                    <label className='form-label'>Content</label>
+                    <input
+                        value={content}
+                        onChange={ (e)=> setContent(e.target.value) }
+                        type="text"
+                        className='form-control'
+                        required
+                    />
+                </div>
+                <button type='submit' onClick={store} className='btn btn-primary'>Store</button>
+            </form>
         </div>
     )
 }
